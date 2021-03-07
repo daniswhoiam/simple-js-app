72ead487ce4a7d497504b4dde471db7605d8e78c
daniswhoiam <daniil.belazovschi@gmail.com>
2021-03-07 10:42:08 +0100

    Set up a first general dataset as an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+// Setting up a general dataset
+let pokemonList = [
+  {
+    name: 'Weedle',
+    height: 0.3,
+    types: [
+      'bug',
+      'poison'
+    ]
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: [
+      'electric'
+    ]
+  },
+  {
+    name: 'Bellsprout',
+    height: 0.7,
+    types: [
+      'grass',
+      'poison'
+    ]
+  }
+];
